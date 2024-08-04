13dcdc7e474f06e73d17dcb7ae62a3a274118bfb
Mohamed Hassan <mohahassanna933@gmail.com>
2024-08-04 11:43:09 +0300

    connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'app.dart';
+import 'firebase_options.dart';
 import 'utils/routing/app_router.dart';
 
-void main() {
+Future<void> main() async {
+  // Add Widgets Binding
+  // Init Local Storage
+  // Init Payment Method
+  // Await Native splash screen
+  // Init Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (value) => runApp(App(appRouter: AppRouter())),
+  );
+  // Init Authentication
+
+  // Load all the Material Design/ Themes/ Localization/ Binding
   runApp(App(appRouter: AppRouter()));
 }
